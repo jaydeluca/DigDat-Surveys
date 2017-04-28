e72d0220ffc5cdb24c922d0cb483cea89fda6cd0
Jay DeLuca <jaydeluca4@gmail.com>
2017-04-27 21:25:52 -0400

    question test

---
+++ tests/Unit/QuestionTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class QuestionTest extends TestCase
+{
+
+    use DatabaseMigrations;
+
+    /** @test */
+    public function a_question_belongs_to_a_survey()
+    {
+        $survey = factory('App\Survey')->create();
+        $question = factory('App\Questions')->create(['survey_id' => $survey->id]);
+        $this->assertEquals($question->survey->name, $survey->name);
+    }
+}
