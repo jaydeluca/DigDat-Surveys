449d6ee41b5632aa687df91c5b210a0991f0eaae
Jay DeLuca <jaydeluca4@gmail.com>
2017-05-24 22:22:02 -0400

    playing with navbar styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,8 +64,10 @@
             <div class="nav-right nav-menu">
 
                 @if (Auth::guest())
+                    <a class="nav-item is-tab" href="/surveys">Browse Surveys</a>
                     <a class="nav-item is-tab" href="{{ route('login') }}">Login</a>
                     <a class="nav-item is-tab" href="{{ route('register') }}">Register</a>
+                    <a class="button c-btn c-btn--primary">Create a Survey</a>
                 @else
                     <p class="nav-item">
                         {{ Auth::user()->name }}
