99980a140e12d700db34c2131d9714927621a520
jesse <jessedp@gmail.com>
2017-10-26 17:38:37 -0400

    add the mysql password from the .env

--- setup.sh
+++ setup.sh
@@ -11,6 +11,6 @@ fi
 composer install
 yarn
 echo "\nEnter mysql root password to create the database if it doesn't exist"
-mysql -u root -p -e 'CREATE DATABASE IF NOT EXISTS dd_surveys'
+mysql -u root -p"$(grep -E 'DB_PASSWORD=(.*)' .env | sed 's/DB_PASSWORD=//1')" -e 'CREATE DATABASE IF NOT EXISTS dd_surveys'
 php artisan migrate
 php artisan db:seed
