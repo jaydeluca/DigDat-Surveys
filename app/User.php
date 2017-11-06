7f2b171a5500644dddb38a314d1d43ec581ca898
jesse <jessedp@gmail.com>
2017-11-05 20:07:46 -0500

    make sure we save a web friendly slug (more later)

--- app/User.php
+++ app/User.php
@@ -31,6 +31,15 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    /** somethings we want to run without Observers/ServiceProviders  */
+    public static function boot()
+    {
+        self::creating(function($u){
+            $u->slug = str_slug($u->slug);
+        });
+
+    }
+
     /**
      * Surveys the user created / owns
      *
@@ -55,6 +64,16 @@ class User extends Authenticatable
     public function getCreatedAtAttribute($value)
     {
         return $this->attributes['created_at'] = Carbon::parse($value)->diffForHumans();
+    }
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
     }
 
 }
