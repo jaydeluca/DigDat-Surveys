ef7ebd8285c86b5f11903582b38fc4e3c7f82954
jesse <jessedp@gmail.com>
2018-01-21 20:43:31 -0500

    make failed login attempts return proper errors (mostly for testing)

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Lang;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+use App\User;
 
 class LoginController extends Controller
 {
@@ -27,6 +31,7 @@ class LoginController extends Controller
      */
     protected $redirectTo = '/home';
 
+    
     /**
      * Create a new controller instance.
      *
@@ -35,5 +40,33 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest', ['except' => 'logout']);
+    }
+
+    /**
+     * Method override to send correct error messages
+     * Get the failed login response instance.
+     *
+     * @param \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+    */
+    protected function sendFailedLoginResponse(Request $request)
+    {
+
+        if (!User::where('email', $request->email)->first()) {
+            return redirect()->back()
+                ->withInput($request->only($this->username(), 'remember'))
+                ->withErrors([
+                    $this->username() => Lang::get('auth.email'),
+                ]);
+        }
+
+        if (!User::where('email', $request->email)->where('password', bcrypt($request->password))->first()) {
+            return redirect()->back()
+                ->withInput($request->only($this->username(), 'remember'))
+                ->withErrors([
+                    'password' => Lang::get('auth.password'),
+                ]);
+        }
+
     }
 }
