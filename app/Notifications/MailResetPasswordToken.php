46b5a87c7160d60a24107f6a571f950a829ab365
jesse <jessedp@gmail.com>
2018-01-25 16:07:16 -0500

    add a custom mailer for password resets

---
+++ app/Notifications/MailResetPasswordToken.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class MailResetPasswordToken extends Notification
+{
+    use Queueable;
+
+    public $token;
+    public $email;
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($email, $token)
+    {
+        $this->email = $email;
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $b64email = base64_encode($this->email);
+        return (new MailMessage)
+            ->subject(config('app.name'). ': Reset Password')
+            ->line('You are receiving this email because we received a password reset request for your account.')
+            ->action('Reset Password', url(config('app.url') . route('password.reset', [$b64email, $this->token], false)))
+            ->line('If you did not request a password reset, no further action is required.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
